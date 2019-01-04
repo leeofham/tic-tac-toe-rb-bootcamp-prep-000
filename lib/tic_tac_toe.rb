fadba4e899e4edc85e13d3862ed66f36de4e0420
Liam Roberts <liam_r_123@hotmail.co.uk>
2019-01-04 12:47:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,20 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  counter = 0
+
+  board.each do |turn|
+    if turn != " "
+      counter += 1
+    end
+    end
+    return counter
+  end
+
+def current_player(board)
+  even_or_odd = turn_count(board) % 2
+
+  even_or_odd == 0 ? "X" : "O"
+end
