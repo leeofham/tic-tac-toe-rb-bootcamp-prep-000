cca66f081573ecc2f60e4391bc51bd742a76311d
Liam Roberts <liam_r_123@hotmail.co.uk>
2019-01-04 12:38:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,8 +21,8 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board, index, player = "X")
-  board[index] = player
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
 def position_taken?(board, index)
